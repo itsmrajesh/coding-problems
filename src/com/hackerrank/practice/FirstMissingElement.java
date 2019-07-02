39723eb2385c8ebb90945531fcf55f97a7e23056
M Rajesh <m.rajeshrajz@gmail.com>
2019-07-02 11:24:24 +0530

    First postive missing element

--- src/com/hackerrank/practice/FirstMissingElement.java
+++ src/com/hackerrank/practice/FirstMissingElement.java
@@ -1,14 +1,14 @@
 package com.hackerrank.practice;
 
 import java.util.Arrays;
-
+//find the next postive missing number in the given array 
+// Test case arr={-5,-5,4,1,2} function should return 3 as missing number
 public class FirstMissingElement {
 
 	public static void main(String[] args) {
 		int arr[] = { 3, 4, -1, 1 };
 		int missing = getFirstMissingNumber(arr);
-		System.out.println("missing element");
-		System.out.println(missing);
+		System.out.println("Missing element : "+missing);
 	}
 
 	private static int getFirstMissingNumber(int[] arr) {
