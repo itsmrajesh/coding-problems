c861b971f21c8bba66717a5a5be1efea2cab62c8
M Rajesh <m.rajeshrajz@gmail.com>
2019-08-08 00:09:54 +0530

    First missing number

--- src/com/leetcode/MissingNumber.java
+++ src/com/leetcode/MissingNumber.java
@@ -1,20 +1,41 @@
 package com.leetcode;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class MissingNumber {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int arr[] = new int[n];
-		for (int i = 0; i < n; i++)
+		System.out.println();
+		int size = sc.nextInt();
+		int arr[] = new int[size];
+		for (int i = 0; i < size; i++)
 			arr[i] = sc.nextInt();
+		Arrays.sort(arr);
+		int missingNumber = getMissing(arr, size);
+		System.out.println(missingNumber);
+	}
 
-		int orginalSum = n * (n + 1) / 2, arrSum = 0;
-		for (int ele : arr)
-			arrSum += ele;
-		System.out.println(orginalSum - arrSum);
+	public static int getMissing(int[] arr, int n) {
+		int res;
+		if (n <= 1) {
+			return arr[0] > 1 ? arr[0] + 1 : 1;
+		}
+		if (arr[0] > 1) {
+			return 1;
+		}
+		for (int i = 0; i < n - 1; i++) {
+			if (Math.abs(arr[i + 1]) - Math.abs(arr[i]) > 1) {
+				res = arr[i + 1] - 1;
+				if (res > 0) {
+					return res;
+				}
+			}
+		}
+
+		return arr[n - 1] + 1;
+
 	}
 
 }
