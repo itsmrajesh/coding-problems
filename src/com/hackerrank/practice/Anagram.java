dca87c925b2824ccf7ec4db5e200eb512ce83727
M Rajesh <m.rajeshrajz@gmail.com>
2019-08-10 01:32:35 +0530

    Added anagarm

--- src/com/hackerrank/practice/Anagram.java
+++ src/com/hackerrank/practice/Anagram.java
@@ -1,12 +1,23 @@
 package com.hackerrank.practice;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class Anagram {
 
 	public static void main(String[] args) {
-			
+		Scanner sc = new Scanner(System.in);
+		String s1 = sc.next();
+		String s2 = sc.next();
+		char[] ch1 = s1.toCharArray();
+		char[] ch2 = s2.toCharArray();
+		s1 = getSorted(ch1);
+		s2 = getSorted(ch2);
+		System.out.println(s1.equals(s2) ? "Anagram" : "Not Anagram");
+	}
+
+	private static String getSorted(char[] ch) {
+		Arrays.sort(ch);
+		return String.valueOf(ch);
 	}
 
 }
