e41f2e69685326a26aefaa14da50fa3b1bf0b101
M Rajesh <m.rajeshrajz@gmail.com>
2019-08-25 19:47:13 +0530

    added mars signal transmission problem hacker rank

--- src/com/hackerrank/practice/MarsVolation.java
+++ src/com/hackerrank/practice/MarsVolation.java
@@ -3,8 +3,27 @@ package com.hackerrank.practice;
 public class MarsVolation {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String s = "SOSOOSOSOSOSOSSOSOSOSOSOSOS";
+		System.out.println(s);
+		int res = marsExploration(s);
+		System.out.println(res);
+	}
 
+	private static int marsExploration(String s) {
+		int count = 0, len = s.length();
+		for (int i = 0; i <= len - 3; i = i + 3) {
+			if (s.charAt(i) != 'S') {
+				count++;
+			}
+			if (s.charAt(i + 1) != 'O') {
+				count++;
+			}
+			if (s.charAt(i + 2) != 'S') {
+				count++;
+			}
+		}
+
+		return count;
 	}
 
 }
