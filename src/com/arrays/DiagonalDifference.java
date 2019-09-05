c93f5318536365bc0237a84026f56c68e78b86e8
M Rajesh <m.rajeshrajz@gmail.com>
2019-09-05 22:33:21 +0530

    added program to find diagonalDifference program in a square matrix

---
+++ src/com/arrays/DiagonalDifference.java
@@ -0,0 +1,21 @@
+package com.arrays;
+
+public class DiagonalDifference {
+	/*Find the diagonal difference of 2D Square matrix  */
+
+	public static void main(String[] args) {
+			int arr[][]= {{1,2,3},{4,5,6},{7,8,9}};
+			int res =  getDiff(arr);
+	}
+
+	private static int getDiff(int[][] arr) {
+		int n=arr.length,lSum=0,rSum=0;
+		for(int i=0;i<n;i++) {
+			lSum += arr[i][i];
+			rSum += arr[i][n-i-1];
+		}
+		System.out.println(lSum+" "+rSum);
+		return 0;
+	}
+
+}
