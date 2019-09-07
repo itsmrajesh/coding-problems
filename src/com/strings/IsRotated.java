e5b30c038ced0ad53d52d2333bd8d1891d489862
M Rajesh <m.rajeshrajz@gmail.com>
2019-09-07 23:15:34 +0530

    added isRotated problem

---
+++ src/com/strings/IsRotated.java
@@ -0,0 +1,26 @@
+package com.strings;
+
+import java.util.Collections;
+import java.util.Comparator;
+
+public class IsRotated {
+
+	public static void main(String[] args) {
+			String s1="amazon";
+			String s2="azonam";
+			int res =  isRotated(s1,s2);
+	}
+
+	private static int isRotated(String s1, String s2) {
+		if(s1.length() != s2.length()) {
+			return 0;
+		}
+		
+		return 0;
+	}
+	
+	
+	
+	//private static 
+
+}
