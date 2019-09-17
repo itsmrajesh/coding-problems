856ba6f9257b970e69e16c179d952607ae9c7a7a
M Rajesh <m.rajeshrajz@gmail.com>
2019-09-18 01:07:01 +0530

    added majorityElement in a arry program

---
+++ src/com/arrays/MajorityElement.java
@@ -0,0 +1,19 @@
+package com.arrays;
+
+public class MajorityElement {
+	/*
+	 * Given an array A of N elements. Find the majority element in the array. A
+	 * majority element in an array A of size N is an element that appears more than
+	 * N/2 times in the array.
+	 * 
+	 * INPUT: N = 5 , Array = 3 1 3 3 2
+	 * 
+	 * OUTPUT: 3 
+	 * 
+	 *  Explanation:Since, 3 is present more than N/2 times, so it is the majority element.
+	 */
+	public static void main(String[] args) {
+				
+	}
+
+}
