416a761b66ce041c5fe85b0856b6f474b352fc74
M Rajesh <m.rajeshrajz@gmail.com>
2020-01-12 22:51:35 +0530

    added Denominations program

---
+++ src/com/numbers/Denominations.java
@@ -0,0 +1,17 @@
+package com.numbers;
+
+public class Denominations {
+
+	public static void main(String[] args) {
+		int n = 5698;
+		String num = String.valueOf(n);
+		int size = num.length()-1, count = 1;
+		for(char ch : num.toCharArray()) {
+			int digit = Integer.parseInt(""+ch);
+			int res = (int)Math.pow(10, size--);
+			System.out.println(digit+"*"+res+" = "+(digit*res));
+		}
+
+	}
+
+}
